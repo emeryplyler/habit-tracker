1d1fea6f56ff8bc2aef047d444ae873f7d688177
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-03-11 16:59:48 -0700

    create habit update user
    successfully updated user's habits array

--- src/app/api/habits/create/route.ts
+++ src/app/api/habits/create/route.ts
@@ -1,6 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { Habit } from "@/models/habitModel";
+import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
 connect();
@@ -11,8 +12,9 @@ export async function POST(request: NextRequest) {
         // use request body to fill in new habit parameters
         const reqBody = await request.json();
         const { name, description, frequency, notes, difficulty, goalCount } = reqBody;
+        const user = await User.findById(userID);
 
-        if (!userID) {
+        if (!userID || !user) {
             return NextResponse.json(
                 { error: "Couldn't verify user is logged in" },
                 { status: 400 }
@@ -21,16 +23,19 @@ export async function POST(request: NextRequest) {
 
         const newHabit = new Habit({
             name,
-            description, 
-            frequency, 
-            notes, 
-            difficulty, 
+            description,
+            frequency,
+            notes,
+            difficulty,
             goalCount,
             completeCount: 0,
             user: userID
         });
 
         const savedHabit = await newHabit.save();
+        // also insert new habit into user's habits array
+        const userUpdate = await User.findByIdAndUpdate(userID, { $push: { habits: savedHabit._id } });
+        console.log(userUpdate);
 
         return NextResponse.json(
             { message: "Habit created successfully", data: savedHabit },
