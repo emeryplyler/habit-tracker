edabef237b1743ece935812715c317a3f9ee3482
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-03-20 13:47:07 -0700

    add update form

--- src/app/habits/[id]/edit/page.tsx
+++ src/app/habits/[id]/edit/page.tsx
@@ -27,6 +27,10 @@ export default function HabitEditPage() {
         }
     };
 
+    const updateHabit = async () => {
+        // call db and update
+    }
+
     useEffect(() => { getHabit(); }, []); // retrieve habit on page load
 
     return (
@@ -34,10 +38,61 @@ export default function HabitEditPage() {
             <h1>Edit Habit</h1>
             {loading && <p>Loading...</p>}
             {habit && !loading && (
-                <div>
-                    <p>{habit.name}</p>
-                    <p>{habit.description}</p>
-                </div>
+                <form action={updateHabit}>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        id="name"
+                        type="text"
+                        value={habit.name}
+                        required
+                        onChange={(e) => setHabit({ ...habit, name: e.target.value })}
+                    />
+
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        id="description"
+                        value={habit.description}
+                        onChange={(e) => setHabit({ ...habit, description: e.target.value })}
+                    />
+
+                    <p>Frequency
+                        <label>
+                            <input type="radio" name="frequency" value="daily" onChange={() => setHabit({ ...habit, frequency: "daily" })} />
+                            Daily
+                        </label>
+                        <label>
+                            <input type="radio" name="frequency" value="weekly" onChange={() => setHabit({ ...habit, frequency: "weekly" })} />
+                            Weekly
+                        </label>
+                    </p>
+
+                    <label htmlFor="notes">Notes</label>
+                    <textarea
+                        id="notes"
+                        value={habit.notes}
+                        onChange={(e) => setHabit({ ...habit, notes: e.target.value })}
+                    />
+
+                    <label htmlFor="difficulty">Difficulty</label>
+                    <select name="difficulty" id="difficulty" value={habit.difficulty || ""} onChange={(e) => setHabit({ ...habit, difficulty: e.target.value })}>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
+
+                    <label htmlFor="goal">Goal</label>
+                    <p>How many times per cycle do you want to do this?</p>
+                    <input
+                        id="goal"
+                        type="number"
+                        value={habit.goalCount}
+                        onChange={(e) => setHabit({ ...habit, goalCount: parseInt(e.target.value) })}
+                    />
+
+                    <button type="submit">Update Habit</button>
+                </form>
             )}
             {!habit && !loading && <p>Habit not found</p>}
         </div>
