9e04ec4fc2e64f67c752b50d503f748b08dc101c
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-03-26 15:40:10 -0700

    add complete count to post
    completecount is currently required in Habit interface; initialize with value of 0

--- src/app/api/habits/create/route.ts
+++ src/app/api/habits/create/route.ts
@@ -17,6 +17,7 @@ export async function POST(request: NextRequest) {
             notes,
             difficulty,
             goalCount,
+            completeCount: 0,
             userId: userID
         }
 
