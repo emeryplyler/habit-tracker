b1905c8d1d5c276c52bfc951c6884085fa983818
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-04-10 15:06:13 -0700

    mockingoose notes

--- __tests__/services/habitService.test.ts
+++ __tests__/services/habitService.test.ts
@@ -28,13 +28,13 @@ describe('test habitService', () => {
   test('getHabitById: service returns habit, ', async () => {
 
     // Arrange
-    mockingoose(HabitModel).toReturn(testModel, 'findOne');
+    mockingoose(HabitModel).toReturn(testModel, 'findOne'); // make findOne always return testModel, no matter the input
 
     // Act
-    const actual: Habit = await getHabitById("111");
+    const actual: Habit = await getHabitById("111"); // try to retrieve habit
 
     // Assert
-    expect(actual.difficulty).toBe(3);
+    expect(actual.difficulty).toBe(3); // check if returned habit matches testModel
     expect(actual.description).toBe('some description');
     expect(actual.frequency).toBe('daily');
   });
@@ -42,12 +42,12 @@ describe('test habitService', () => {
   test('getHabitById: goalStatus default to incomplete ', async () => {
 
     // Arrange
-    mockingoose(HabitModel).toReturn(testModel, 'findOne');
+    mockingoose(HabitModel).toReturn(testModel, 'findOne'); // arrange again
 
     // Act
     const actual: Habit = await getHabitById("111111111111111111111111");
 
     // Assert
-    expect(actual.goalStatus).toBe('incomplete');
+    expect(actual.goalStatus).toBe('incomplete'); // testModel does not have goalStatus, so it should default to 'incomplete'; that logic is in habitService.ts
   });
 });
