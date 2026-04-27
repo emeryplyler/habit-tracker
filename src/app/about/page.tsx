fa63c33a53e8871b595341dce96ad50f3b33efbe
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-04-27 16:16:16 -0700

    style about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import { Toaster } from "react-hot-toast";
+import styles from "@/styles/AboutPage.module.css";
 
 export default function AboutPage() {
 
     return (
-        <div className="about-page">
-            <div>
+        <div className={styles.aboutPage}>
+            <div className={styles.piece}>
                 <h1>Welcome to Habit Tracker!</h1>
                 <p>
-                    This is a lightweight online tool to help you keep track of all your daily and weekly tasks to build up healthy habits.
+                    This is a free, lightweight online tool to help you keep track of all your daily and weekly tasks to build up healthy habits.
                     You can add habits, set goals, and then check them off when you complete them.
                 </p>
                 <p>
                     When creating a habit, you can add additional information like a description, notes, and difficulty level to help you stay motivated.
                 </p>
             </div>
-            <div>
+            <div className={styles.piece}>
                 <h2>About</h2>
                 <p>
                     This app was built using Node.js and the Next.js framework, with a MongoDB database for storing habit and user information.
